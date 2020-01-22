c8e9a4a44b02c20d3992f50f7ed42dc843565a09
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2020-01-22 15:50:36 +0700

    Changed Card to be interface

--- Assets/scripts/Card/AppearDisappearCard.cs
+++ Assets/scripts/Card/AppearDisappearCard.cs
@@ -1,64 +1,24 @@
 ﻿using UnityEngine;
 using Vuforia;
 
-public class AppearDisappearCard : MonoBehaviour, ITrackableEventHandler
+public class AppearDisappearCard : Card
 {
 
-    [SerializeField] GameObject particleGraphics;
-    GameObject tempParticle;
-
-    private TrackableBehaviour mTrackableBehaviour;
-
-    private void Start()
-    {
-        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
-        if (mTrackableBehaviour)
-        {
-            mTrackableBehaviour.RegisterTrackableEventHandler(this);
-        }
-    }
-
-    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
-    {
-        if (newStatus == TrackableBehaviour.Status.DETECTED ||
-
-            newStatus == TrackableBehaviour.Status.TRACKED)
-
-        {
-
-            OnTrackingFound();
-
-        }
-
-        else
-
-        {
-
-            OnTrackingLost();
-
-        }
-    }
-
-    private void Update()
-    {
-        //TODO: Countdown the cooldown here.
-    }
-
-    public void OnTrackingFound()
+    public override void OnTrackingFound()
     {
         Debug.Log("Tracking");
         Debug.Log("Create particle");
-        tempParticle = Instantiate(particleGraphics, transform);
+        particleGraphics = Instantiate(particlePrefab, transform);
         //TODO: Attack and cooldown here? It will not attack again if the card is not lost the tracking even if the cooldown is done.
     }
 
-    public void OnTrackingLost()
+    public override void OnTrackingLost()
     {
         Debug.Log("Untracking");
-        if (tempParticle != null)
+        if (particleGraphics != null)
         {
             Debug.Log("Destroy particle");
-            Destroy(tempParticle);
+            Destroy(particleGraphics);
             //TODO: Release cooldown or do something when it is gone?
         }
     }
