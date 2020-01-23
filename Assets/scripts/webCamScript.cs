815a1d2908fe5bfe9cb028e89bb4eaae32d128ae
masty123 <pwnimatorz@hotmail.com>
2020-01-23 20:30:40 +0700

    save cs file

--- Assets/scripts/webCamScript.cs
+++ Assets/scripts/webCamScript.cs
@@ -8,7 +8,9 @@ public class webCamScript : MonoBehaviour
     public GameObject webCameraPlane;
     void Start()
     {
-        
+        WebCamTexture webCameraTexture = new WebCamTexture();
+        webCameraPlane.GetComponent<MeshRenderer>().material.mainTexture = webCameraTexture;
+        webCameraTexture.Play();
     }
 
     // Update is called once per frame
