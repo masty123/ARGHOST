a745beab31384e7e80f58275b3af38b0ae386b59
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2020-01-23 10:59:06 +0700

    Fixed Cooldown

--- Assets/scripts/Card/AppearDisappearCard.cs
+++ Assets/scripts/Card/AppearDisappearCard.cs
@@ -6,21 +6,23 @@ public class AppearDisappearCard : Card
 
     public override void OnTrackingFound()
     {
-        Debug.Log("Tracking");
-        Debug.Log("Create particle");
-        particleGraphics = Instantiate(particlePrefab, transform);
-        //TODO: Attack and cooldown here? It will not attack again if the card is not lost the tracking even if the cooldown is done.
+        base.OnTrackingFound();
+        //Debug.Log("Tracking");
+        //Debug.Log("Create particle");
+        //particleGraphics = Instantiate(particlePrefab, transform);
+        ////TODO: Attack and cooldown here? It will not attack again if the card is not lost the tracking even if the cooldown is done.
     }
 
     public override void OnTrackingLost()
     {
-        Debug.Log("Untracking");
-        if (particleGraphics != null)
-        {
-            Debug.Log("Destroy particle");
-            Destroy(particleGraphics);
-            //TODO: Release cooldown or do something when it is gone?
-        }
+        base.OnTrackingLost();
+        //Debug.Log("Untracking");
+        //if (particleGraphics != null)
+        //{
+        //    Debug.Log("Destroy particle");
+        //    Destroy(particleGraphics);
+        //    //TODO: Release cooldown or do something when it is gone?
+        //}
     }
 
 }
