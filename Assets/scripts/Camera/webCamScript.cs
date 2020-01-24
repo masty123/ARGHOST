ede8eabd31e113f4cff4ccecc843e17d4e7aa861
masty123 <theeruth.b@ku.th>
2020-01-24 12:54:26 +0700

    tried AR camera without gyroscope

--- Assets/scripts/Camera/webCamScript.cs
+++ Assets/scripts/Camera/webCamScript.cs
@@ -8,18 +8,23 @@ public class webCamScript : MonoBehaviour
     public GameObject webCameraPlane;
     void Start()
     {
+        //Check if its a mobile platform
         if(Application.isMobilePlatform)
         {
+            //Create new gameobject called camera parent
             GameObject cameraParent = new GameObject("camParent");
+            //Set cameraParent position as the same as this transform.
             cameraParent.transform.position = this.transform.position;
+            //parent relationship
             this.transform.parent = cameraParent.transform;
+            //camera rotation
             cameraParent.transform.Rotate(Vector3.right, 90);
         }
 
         Input.gyro.enabled = true;
 
 
-        WebCamTexture webCameraTexture = new WebCamTexture(1920,1080, 30);
+        WebCamTexture webCameraTexture = new WebCamTexture();
         webCameraPlane.GetComponent<MeshRenderer>().material.mainTexture = webCameraTexture;
         webCameraTexture.Play();
     }
@@ -27,6 +32,7 @@ public class webCamScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Quaternion as gyro rotation;
         Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y, Input.gyro.attitude.z, -Input.gyro.attitude.w);
         this.transform.localRotation = cameraRotation;
     }
