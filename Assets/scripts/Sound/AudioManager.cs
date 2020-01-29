ea52233a69c26bfd37dcba0fbd45fe5cc076b8ee
masty123 <theeruth.b@ku.th>
2020-01-29 14:29:43 +0700

    add footstep sound for enemy

---
+++ Assets/scripts/Sound/AudioManager.cs
@@ -0,0 +1,53 @@
+﻿ using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+//For Ambient, menu theme, non object related only.
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null) { instance = this; }
+        else { Destroy(gameObject); return; }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source =  gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        //Do some ambient scary theme I guess.
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: "+ name+" not found!");
+            return;
+        }
+           
+        s.source.Play();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
