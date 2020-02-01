301277cd582b92623ac090c0af8464090dcf8e62
Nitith Chayakul <c.nitith@gmail.com>
2020-02-01 20:17:05 +0700

    add health bar

--- Assets/scripts/Player/PlayerManager.cs
+++ Assets/scripts/Player/PlayerManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -16,4 +14,62 @@ public class PlayerManager : MonoBehaviour
     #endregion
 
     public GameObject player;
+
+    float maxHP = 100f;
+    float hp;
+    float maxMana = 100f;
+    float mana;
+
+    void Start()
+    {
+        hp = maxHP;
+        mana = maxMana;
+    }
+
+    void Update()
+    {
+        Damage(.1f);
+    }
+
+    public void Damage(float dmg)
+    {
+        hp -= dmg;
+        if(hp <= 0) 
+        {
+            GameOver();
+        }
+    }
+
+    public void ManaUse(float m)
+    {
+        if(m <= mana)
+        {
+            mana -= m;
+        }
+    }
+
+    public float GetHealth()
+    {
+        return hp;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHP;
+    }
+
+    public float GetMana()
+    {
+        return mana;
+    }
+
+    public float GetMaxMana()
+    {
+        return maxMana;
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over!!");
+    }
 }
