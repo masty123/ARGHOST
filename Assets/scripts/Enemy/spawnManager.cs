15ae115bea2955c8438bf978d1bc0f583442bb91
Nitith Chayakul <c.nitith@gmail.com>
2020-03-09 14:31:22 +0700

    delete camera log

--- Assets/scripts/Enemy/spawnManager.cs
+++ Assets/scripts/Enemy/spawnManager.cs
@@ -93,7 +93,6 @@ public class spawnManager : MonoBehaviour
     //Remove an enemy from the list if one dies.
     private void Update()
     {
-        Debug.Log("Camera position = " + cameraTransform.position);
         if (state == SpawnState.WAITING)
         {
             RemoveDestroyedEnemy();
