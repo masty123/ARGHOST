8259e94e407fea966cc57e6e5d0ef0c62e1ad914
masty123 <theeruth.b@ku.th>
2020-03-09 11:34:51 +0700

    optimize jumpScareV2 script

--- Assets/Scripts/Camera/jumpScareV2.cs
+++ Assets/Scripts/Camera/jumpScareV2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class jumpScareV2 : MonoBehaviour
 {
-    public GameObject mainCamera;
+    //public GameObject mainCamera;
     public GameObject ghostObj;
 
     public bool jumpScare;
@@ -14,7 +14,7 @@ public class jumpScareV2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = GameObject.FindWithTag("MainCamera");
+        //mainCamera = GameObject.FindWithTag("MainCamera");
     }
 
      
@@ -41,8 +41,8 @@ public class jumpScareV2 : MonoBehaviour
         //ghost.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z + distance);
         ghost.transform.position = transform.position + transform.forward * distance;
         ghost.transform.rotation = new Quaternion(0.0f, transform.rotation.y, 0.0f, transform.rotation.w);
-        ghost.transform.LookAt(mainCamera.transform);
-        ghost.transform.parent = mainCamera.transform;
+        ghost.transform.LookAt(transform);
+        ghost.transform.parent = transform;
     }
 
     //Check if enemy hit player's box collider
