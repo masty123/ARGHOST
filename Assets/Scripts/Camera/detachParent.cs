9247e40a51015bd7efbded4d4409e6a32fc1aa8c
masty123 <theeruth.b@ku.th>
2020-04-02 10:04:43 +0700

    fix gyroscope rotation menu

---
+++ Assets/Scripts/Camera/detachParent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detachParent : MonoBehaviour
+{
+    [SerializeField] float seconds;
+    private bool detachChild;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(DetachParent());
+    }
+
+    IEnumerator DetachParent()
+    {
+        yield return new WaitForSeconds(seconds);
+        transform.parent = null;
+        
+    }
+}
