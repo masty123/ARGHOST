4a0e681695e00c5d5da91066e7dceb45ecf1d1ca
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2020-04-06 17:18:47 +0700

    Added local savefile system

---
+++ Assets/scripts/SaveAndScore/Savefile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Savefile
+{
+
+    public string PlayerName = "";
+    public int HighScore = 0;
+    public int Gold = 0;
+    public Dictionary<string,int> Upgrades;
+
+    public Savefile()
+    {
+        Upgrades = new Dictionary<string, int>();
+    }
+
+}
